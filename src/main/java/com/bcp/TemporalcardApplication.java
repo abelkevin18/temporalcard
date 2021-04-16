64d3fd43169c3335a24e64932f39a35fb127a7ad
abel <abelkevin15@gmail.com>
2021-04-16 13:56:46 -0500

    first commit

---
+++ src/main/java/com/bcp/TemporalcardApplication.java
@@ -0,0 +1,13 @@
+package com.bcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalcardApplication.class, args);
+	}
+
+}
